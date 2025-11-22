3c13a1dc953e511e5c0dcafe4b570685a5b0bb1c
El0Lobo <95318796+El0Lobo@users.noreply.github.com>
2025-11-22 05:35:08 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
 @echo off
-npm start
+setlocal
+cd /d "%~dp0"     rem switch to this folder
+call npm install  rem skip if you already installed deps
+call npm run start
 pause
+
+C:\Windows\System32\cmd.exe /k "cd /d C:\Users\Lobo\Documents\GitHub\Soundbot && npm run start"
